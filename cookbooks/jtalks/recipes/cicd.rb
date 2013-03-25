099e9c370016e2dd018f76f95f1476ee87deee44
sbashkyrtsev <sbashkyrtsev@griddynamics.com>
2013-03-25 16:54:27 +0400

    added vagrant environment

--- cookbooks/jtalks/recipes/cicd.rb
+++ cookbooks/jtalks/recipes/cicd.rb
@@ -10,3 +10,14 @@ python_pip "jtalks-cicd" do
   action :install
 end
 
+#copy default vagrant configuration from COOKBOOK/files
+remote_directory "/home/vagrant/.jtalks" do
+  source ".jtalks"
+  files_owner "vagrant"
+  files_group "vagrant"
+  files_mode 00660
+  owner "vagrant"
+  group "vagrant"
+  recursive true
+  mode 00777
+end
