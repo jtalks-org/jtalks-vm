6acc2e84669d61ea964ab4c853b129fd532ab24d
Maksim Rusan <maksim.rusan@strevus.com>
2014-04-08 15:18:26 +0300

    Added cookbook name and version bump

--- cookbooks/tomcat7/metadata.rb
+++ cookbooks/tomcat7/metadata.rb
@@ -1,9 +1,10 @@
+name             'tomcat7'
 maintainer       "Vladislav Mikhaylov"
 maintainer_email "solarvm@gmail.com"
 license          "AGPL"
 description      "Installs/Configures tomcat7 binary distrib"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 
 recipe "tomcat7::default", "Installs and configures Tomcat7"
 
